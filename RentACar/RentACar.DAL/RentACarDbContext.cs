7705eddf69ddcf4127ff559ce9cebe5725418e5e
Marko Zaninović <mzaninovic555@gmail.com>
2022-05-30 14:55:27 +0200

    Dodane biblioteke za entityframework.identity.

---
+++ RentACar/RentACar.DAL/RentACarDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RentACar.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.DAL
+{
+    public class RentACarDbContext : IdentityDbContext<AppUser>
+    {
+        public RentACarDbContext(DbContextOptions<RentACarDbContext> options) : base(options){ }
+    }
+}
--- RentACar/RentACar/Program.cs
+++ RentACar/RentACar/Program.cs
@@ -1,17 +1,30 @@
+
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using RentACar.DAL;
 using RentACar.Data;
+using RentACar.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<RentACarDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("RentACarDbContext"),
+            opt => opt.MigrationsAssembly("RentACar.DAL")));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddRoleManager<RoleManager<IdentityRole>>()
+    .AddDefaultUI()
+    .AddDefaultTokenProviders()
+    .AddEntityFrameworkStores<RentACarDbContext>();
+
+builder.Services.AddScoped<UserManager<AppUser>>();
+
+//TODO: Google autentikacija
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -35,6 +48,13 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+    endpoints.MapRazorPages();
+});
+
 
 app.MapControllerRoute(
     name: "default",
