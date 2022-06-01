83d356827ba44b1a18529362234f2e5c17e87601
Marko Zaninović <mzaninovic555@gmail.com>
2022-06-01 17:13:40 +0200

    Dodane migracije i podatci za lokacije i prodavaone

---
+++ RentACar/RentACar.DAL/Migrations/20220601151120_addedLocations.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentACar.DAL.Migrations
+{
+    public partial class addedLocations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "ID", "Name" },
+                values: new object[] { 1, "Hrvatska" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "ID", "Name" },
+                values: new object[] { 2, "United States" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "ID", "CountryID", "Name", "PostalCode" },
+                values: new object[,]
+                {
+                    { 1, 1, "Zagreb", 10000 },
+                    { 2, 1, "Rijeka", 51000 },
+                    { 3, 1, "Split", 21000 },
+                    { 4, 1, "Dubrovnik", 20000 },
+                    { 5, 2, "New York", 11368 },
+                    { 6, 2, "Los Angeles", 90011 },
+                    { 7, 2, "Washington D.C.", 20011 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Stores",
+                columns: new[] { "ID", "Address", "CityID", "Email", "OpenFrom", "OpenTo", "PhoneNumber" },
+                values: new object[,]
+                {
+                    { 1, "Vukovarska 41", 1, "rentacar-zagreb@gmail.com", new TimeSpan(0, 6, 0, 0, 0), new TimeSpan(0, 17, 0, 0, 0), "01 3885 777" },
+                    { 2, "Zvonimirova 20 A", 2, "rentacar-rijeka@gmail.com", new TimeSpan(0, 6, 0, 0, 0), new TimeSpan(0, 16, 0, 0, 0), "01 2314 019 " },
+                    { 3, "Vukovarska 182", 3, "rentacar-split@gmail.com", new TimeSpan(0, 8, 0, 0, 0), new TimeSpan(0, 15, 0, 0, 0), "01 3310 050 " },
+                    { 4, "Sinjska 15", 4, "rentacar-dubrovnik@gmail.com", new TimeSpan(0, 7, 0, 0, 0), new TimeSpan(0, 14, 0, 0, 0), "01 4552 590 " },
+                    { 5, "27 E 22nd St, NY 10010", 5, "rentacar-newyork@gmail.com", new TimeSpan(0, 5, 0, 0, 0), new TimeSpan(0, 22, 0, 0, 0), "347 3917 724" },
+                    { 6, "9800 S Sepulveda Blvd, CA 90045", 6, "rentacar-losangeles@gmail.com", new TimeSpan(0, 6, 0, 0, 0), new TimeSpan(0, 21, 0, 0, 0), "916 955 0339" },
+                    { 7, "923 16th St NW, DC 20006", 7, "rentacar-washingtondc@gmail.com", new TimeSpan(0, 8, 0, 0, 0), new TimeSpan(0, 20, 0, 0, 0), "202 714 2570" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Stores",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Stores",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Stores",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Stores",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Stores",
+                keyColumn: "ID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Stores",
+                keyColumn: "ID",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Stores",
+                keyColumn: "ID",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "ID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "ID",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "ID",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "ID",
+                keyValue: 2);
+        }
+    }
+}
--- RentACar/RentACar.DAL/RentACarDbContext.cs
+++ RentACar/RentACar.DAL/RentACarDbContext.cs
@@ -17,5 +17,47 @@ namespace RentACar.DAL
         public DbSet<Country> Countries { get; set; }
         public DbSet<Reservation> Reservations { get; set; }
         public DbSet<Store> Stores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            List<Store> stores = new List<Store>
+            {
+                new Store{ ID = 1, Address = "Vukovarska 41", PhoneNumber = "01 3885 777", Email = "rentacar-zagreb@gmail.com", OpenFrom = TimeSpan.Parse("06:00"), OpenTo = TimeSpan.Parse("17:00"), CityID = 1 },
+                new Store{ ID = 2, Address = "Zvonimirova 20 A", PhoneNumber = "01 2314 019 ", Email = "rentacar-rijeka@gmail.com", OpenFrom = TimeSpan.Parse("06:00"), OpenTo = TimeSpan.Parse("16:00"), CityID = 2 },
+                new Store{ ID = 3, Address = "Vukovarska 182", PhoneNumber = "01 3310 050 ", Email = "rentacar-split@gmail.com", OpenFrom = TimeSpan.Parse("08:00"), OpenTo = TimeSpan.Parse("15:00"), CityID = 3 },
+                new Store{ ID = 4, Address = "Sinjska 15", PhoneNumber = "01 4552 590 ", Email = "rentacar-dubrovnik@gmail.com", OpenFrom = TimeSpan.Parse("07:00"), OpenTo = TimeSpan.Parse("14:00"), CityID = 4 },
+                new Store{ ID = 5, Address = "27 E 22nd St, NY 10010", PhoneNumber = "347 3917 724", Email = "rentacar-newyork@gmail.com", OpenFrom = TimeSpan.Parse("05:00"), OpenTo = TimeSpan.Parse("22:00"), CityID = 5 },
+                new Store{ ID = 6, Address = "9800 S Sepulveda Blvd, CA 90045", PhoneNumber = "916 955 0339", Email = "rentacar-losangeles@gmail.com", OpenFrom = TimeSpan.Parse("06:00"), OpenTo = TimeSpan.Parse("21:00"), CityID = 6 },
+                new Store{ ID = 7, Address = "923 16th St NW, DC 20006", PhoneNumber = "202 714 2570", Email = "rentacar-washingtondc@gmail.com", OpenFrom = TimeSpan.Parse("08:00"), OpenTo = TimeSpan.Parse("20:00"), CityID = 7 }
+            };
+
+            List<City> citiesCroatia = new List<City>
+            {
+                new City { ID = 1, Name = "Zagreb", PostalCode = 10000, CountryID = 1 },
+                new City { ID = 2, Name = "Rijeka", PostalCode = 51000, CountryID = 1 },
+                new City { ID = 3, Name = "Split", PostalCode = 21000, CountryID = 1 },
+                new City { ID = 4, Name = "Dubrovnik", PostalCode = 20000, CountryID = 1 }
+            };
+
+            List<City> citiesUSA = new List<City>
+            {
+                new City { ID = 5, Name = "New York", PostalCode = 11368, CountryID = 2 },
+                new City { ID = 6, Name = "Los Angeles", PostalCode = 90011, CountryID = 2 },
+                new City { ID = 7, Name = "Washington D.C.", PostalCode = 20011, CountryID = 2 }
+            };
+
+            List<Country> countries = new List<Country>
+            {
+                new Country { ID = 1, Name = "Hrvatska" },
+                new Country { ID = 2, Name = "United States" },
+            };
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().HasData(countries);
+            modelBuilder.Entity<Store>().HasData(stores);
+            modelBuilder.Entity<City>().HasData(citiesCroatia);
+            modelBuilder.Entity<City>().HasData(citiesUSA);
+        }
     }
 }
