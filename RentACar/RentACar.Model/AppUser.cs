7dedf0f121574e6f72c7ae006c29ec7530503ac2
Marko Zaninović <mzaninovic555@gmail.com>
2022-06-05 13:10:38 +0200

    Dodana OIB restrikcija

--- RentACar/RentACar.Model/AppUser.cs
+++ RentACar/RentACar.Model/AppUser.cs
@@ -5,6 +5,8 @@ namespace RentACar.Model
 {
     public class AppUser : IdentityUser
     {
+        [Required]
+        [RegularExpression("[0-9]{11}")]
         [StringLength(255)]
         public string OIB { get; set; }
     }
