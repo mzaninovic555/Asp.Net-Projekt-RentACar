ab8c47d916b40bb13c656db2f0e3b25cd97e618a
Marko Zaninović <mzaninovic555@gmail.com>
2022-06-06 17:20:01 +0200

    Dodana create forma.

--- RentACar/RentACar/Controllers/CarController.cs
+++ RentACar/RentACar/Controllers/CarController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RentACar.DAL;
 using RentACar.Model;
@@ -8,10 +10,12 @@ namespace RentACar.Web.Controllers
     public class CarController : Controller
     {
         private RentACarDbContext dbContext;
+        private UserManager<AppUser> userManager;
 
-        public CarController(RentACarDbContext dbContext)
+        public CarController(RentACarDbContext dbContext, UserManager<AppUser> userManager)
         {
             this.dbContext = dbContext;
+            this.userManager = userManager;
         }
 
         [Route("auti/{carID?}")]
@@ -26,6 +30,51 @@ namespace RentACar.Web.Controllers
        
 
             return View("Index", cars.ToList());
+        }
+
+        [ActionName("Create")]
+        public IActionResult Create()
+        {
+            FillDropdownValues();
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        public IActionResult Create(Car model)
+        {
+            if (ModelState.IsValid)
+            {
+                dbContext.Add(model);
+                dbContext.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                FillDropdownValues();
+                return View();
+            }
+        }
+
+
+        private void FillDropdownValues()
+        {
+            var selectItems = new List<SelectListItem>();
+
+            //Polje je opcionalno
+            var listItem = new SelectListItem();
+            listItem.Text = "- odaberite -";
+            listItem.Value = "";
+            selectItems.Add(listItem);
+
+            foreach (var brand in this.dbContext.Brands)
+            {
+                listItem = new SelectListItem(brand.Name, brand.Id.ToString());
+                selectItems.Add(listItem);
+            }
+
+            ViewBag.Brands = selectItems;
         }
     }
 }
